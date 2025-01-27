ec175943afed3eae2d4fd1e86281f3e45c11a7a1
hod25 <hod7112000@gmail.com>
2025-01-27 16:20:09 +0200

    recipe routes

---
+++ src/routes/recipe_routes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+import { RecipeController } from "../controllers/recipe_controller";
+
+//post request to create a new recipe
+router.post("/", RecipeController.createRecipe);
+
+//update request to update a recipe by id
+router.put("/:id", RecipeController.updateRecipe);
+
+//get request to get recipe by id
+router.get("/:id", RecipeController.getRecipe);
+
+//get request to get by user id
+router.get("/:user", RecipeController.getRecipeByUser);
+
+//get request to by tag and title
+router.get("/:tag/:title", RecipeController.getRecipeByTagTitle);
+
