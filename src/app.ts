a76d88412b11dbc387d421d2f55d4bae4f6a6d90
hod25 <hod7112000@gmail.com>
2025-03-17 17:32:08 +0200

    url

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import initApp from "./server";
 const port = process.env.PORT;
+const path = process.env.PATH;
 
 initApp()
   .then((app) => {
     app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`);
+      //console.log(`Example app listening at http://localhost:${port}`);
+      console.log(`Example app listening at http://${path}:${port}`);
+      
     });
   })
   .catch(() => {
