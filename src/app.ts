1a03a2ba62fe54e01a5049bc37fbefbdaf821581
hod25 <hod7112000@gmail.com>
2025-03-18 23:17:39 +0200

    port and catch

--- src/app.ts
+++ src/app.ts
@@ -7,19 +7,23 @@ const port = process.env.PORT;
 const domainBase = process.env.DOMAIN_BASE;
 
 const tmpFunc = async () => {
-  const app = await initApp();
-  if (process.env.NODE_ENV === "production") {
-    app.listen(port, () => {
-      console.log(`Example app listening at ${domainBase}:${port}`);
-    });
-  } else {
-    const httpsOptions = {
-      key: fs.readFileSync("./client-key.pem"),
-      cert: fs.readFileSync("./client-cert.pem"),
-    };
-    https.createServer(httpsOptions, app).listen(port, () => {
-      console.log(`Example app listening at ${domainBase}:${port}`);
-    });
+  try {
+    const app = await initApp();
+    if (process.env.NODE_ENV === "production") {
+      app.listen(port, () => {
+        console.log(`Example app listening at ${domainBase}:${port}`);
+      });
+    } else {
+      const httpsOptions = {
+        key: fs.readFileSync("./client-key.pem"),
+        cert: fs.readFileSync("./client-cert.pem"),
+      };
+      https.createServer(httpsOptions, app).listen(port, () => {
+        console.log(`Example app listening at ${domainBase}:${port}`);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting the server:", error);
   }
 };
 tmpFunc();
